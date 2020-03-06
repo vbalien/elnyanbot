5b3073d3f54379d7202c9ef6c83737bccbce4d4b
Jisu Kim <vbalien@live.jp>
2020-03-07 04:05:51 +0900

    Change countdown delete condition

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,5 @@
 export { default as CommandParser } from "./CommandParser";
 export { default as CountDown } from "./CountDown";
-export { default as Memo } from "./Memo";
+export { default as Memo, DeleteMemoCommand } from "./Memo";
 export { default as Select } from "./Select";
 export { default as Anitable } from "./Anitable";
