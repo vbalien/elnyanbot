903028d4c496d0ce2d9c7deb8123813d86b14bcb
Jisu Kim <webmaster@alien.moe>
2021-02-09 05:09:36 +0900

    Update api

--- src/middleware/SchoolFood/Api.ts
+++ src/middleware/SchoolFood/Api.ts
@@ -1,50 +1,83 @@
 import axios from "axios";
-import { DateTime } from "luxon";
-
-export interface Menu {
-  name: string;
-  foods: string[];
-}
-
-export enum MenuKind {
-  STUDENT = 1,
-  DODAM,
-  SNACK,
-  FOOD_COURT,
-  KITCHEN,
-}
-export const MenuString = [
-  "",
-  "학생식당",
-  "도담식당",
-  "스낵코너",
-  "푸드코트",
-  "더 키친",
-];
 
 const api = axios.create({
-  baseURL: "http://m.soongguri.com/m_req/",
+  baseURL: "https://vbalien.github.io/ssu-today-meals/data",
   timeout: 1000,
 });
 
-export default {
-  getMenu: async (kind: MenuKind): Promise<Menu[]> => {
-    const date = DateTime.local().setZone("Asia/Seoul");
-    const res = await api.get(
-      `/m_menu.php?rcd=${kind}&sdt=${date.toFormat("yyyyMMdd")}`
+interface Place {
+  id: number;
+  name: string;
+}
+
+export const places: Place[] = [
+  {
+    id: 1,
+    name: "학생식당",
+  },
+  {
+    id: 2,
+    name: "숭실도담",
+  },
+  {
+    id: 7,
+    name: "FACULTY LOUNGE",
+  },
+  {
+    id: 4,
+    name: "스넥코너",
+  },
+  {
+    id: 5,
+    name: "푸드코트",
+  },
+  {
+    id: 6,
+    name: "더 키친",
+  },
+];
+
+interface MenuRaw {
+  kind: string;
+  price?: number;
+  image?: string;
+  foods: string;
+}
+
+class Menu {
+  private raw: MenuRaw;
+
+  constructor(raw: MenuRaw) {
+    this.raw = raw;
+  }
+
+  get kind() {
+    return this.raw.kind;
+  }
+
+  get image() {
+    return this.raw.image;
+  }
+
+  get foods() {
+    return this.raw.foods
+      .replace(/<br>|<\/div>/gi, "\n")
+      .replace(/&nbsp;/gi, " ")
+      .replace(/<.*?>/gis, "")
+      .replace(/\n+/gi, "\n");
+  }
+
+  get price() {
+    return (
+      this.raw.price.toString().replace(/\B(?=(\d{3})+(?!\d))/, ",") + "원"
     );
-    const results = [
-      ...res.data.matchAll(
-        /<tr>[^<]*<td class="menu_nm">(?<name>[^]*?)<\/td>[^<]*<td class="menu_list">(?<menu>[^]*?)\s*<\/td>/gi
-      ),
-    ];
-    const menu: Menu[] = results.map((result) => ({
-      name: result.groups.name.trim(),
-      foods: result.groups.menu
-        .split(/<[^>]*>/gi)
-        .map((el: string) => el.trim())
-        .filter((el: string) => el !== "" && el.search(/[a-zA-Z]/gi) === -1),
-    }));
+  }
+}
+
+export default {
+  getMenu: async (place: Place): Promise<Menu[]> => {
+    const res = await api.get(`/${place.id}.json`);
+    const menu: Menu[] = res.data.menus.map((m: MenuRaw) => new Menu(m));
     return menu;
   },
 };
--- src/middleware/SchoolFood/index.ts
+++ src/middleware/SchoolFood/index.ts
@@ -1,38 +1,48 @@
-import { Middleware } from "telegraf";
-import Api, { MenuKind, Menu, MenuString } from "./Api";
 import { KeyboardBuilder } from "../../util";
 import { action, command, middleware } from "../../decorators";
 import { AppContext } from "../../App";
+import Api, { places } from "./Api";
+import { ExtraReplyMessage } from "telegraf/typings/telegram-types";
 
 @middleware()
 export default class SchoolFood {
-  private _makeText(data: Menu) {
-    return `<b>${data.name}</b>\n${data.foods.reduce(
-      (acc, cur) => acc + `${cur}\n`,
-      ""
-    )}\n`;
-  }
-
   @command("schoolfood")
   @action(/^\/schoolfood \d$/)
   async command(ctx: AppContext) {
-    let restaurant = Number.parseInt(ctx.state.command.args);
-    if (Number.isNaN(restaurant)) restaurant = MenuKind.STUDENT;
-    const menus = await Api.getMenu(restaurant);
-    let result = `<b>${MenuString[restaurant]}</b>\n\n`;
-    for (const menu of menus) {
-      result += this._makeText(menu);
+    const placeId =
+      Number.parseInt(ctx.state.command.splitArgs[0]) || places[0].id;
+    const place = places.find((p) => p.id === placeId);
+    const menus = await Api.getMenu(place);
+
+    const replyMarkup = new KeyboardBuilder();
+    const col = 2;
+    for (let i = 0; i < places.length; i += col) {
+      replyMarkup.addRow(
+        places.slice(i, i + col).map((p) => [p.name, `/schoolfood ${p.id}`])
+      );
     }
-    const send = !ctx.callbackQuery ? ctx.reply : ctx.editMessageText;
-    send(result, {
-      reply_markup: new KeyboardBuilder()
-        .addRow(
-          MenuString.slice(1, 3).map((e, i) => [e, `/schoolfood ${i + 1}`])
-        )
-        .addRow(MenuString.slice(3).map((e, i) => [e, `/schoolfood ${i + 3}`]))
-        .addRow([["메시지 지우기", "delmsg"]])
-        .build(),
+    replyMarkup.addRow([["메시지 지우기", "delmsg"]]);
+
+    const options: ExtraReplyMessage = {
+      reply_markup: replyMarkup.build(),
       parse_mode: "HTML",
-    });
+      disable_web_page_preview: true,
+    };
+
+    const send = !ctx.callbackQuery ? ctx.reply : ctx.editMessageText;
+    if (menus.length !== 0)
+      send(
+        menus.reduce(
+          (prev, curr) =>
+            prev +
+            `<b>${curr.kind} - ${
+              curr.price +
+              (curr.image ? ` <a href="${curr.image}">사진</a>` : "")
+            }</b>\n${curr.foods}\n\n`,
+          `<b>${place.name}</b>\n`
+        ),
+        options
+      );
+    else send(`<b>${place.name}</b>\n<b>오늘은 쉽니다.</b>`, options);
   }
 }
