50cd17b189b0810ba7151b7bb7f75cdf11039664
Jisu Kim <webmaster@alien.moe>
2021-06-03 22:48:16 +0900

    klaywatch 직접 계산

--- src/middleware/Klaywatch.ts
+++ src/middleware/Klaywatch.ts
@@ -4,53 +4,70 @@ import { prop, getModelForClass, ReturnModelType } from "@typegoose/typegoose";
 import { KeyboardBuilder } from "../util";
 import axios from "axios";
 
-export interface TotalAmount {
-  krw: string;
-  usd: string;
+interface TokenAddressInfo {
+  success: boolean;
+  code: number;
+  health: number;
+  result: {
+    address: string;
+    balance: string;
+    type: number;
+    creator: string;
+    creationTxHash: string;
+    verificationFlag: boolean;
+    transferCount: number;
+    nftTransferCount: number;
+    txCount: number;
+    eventCount: number;
+    internalTxCount: number;
+    tokenName: string;
+    symbol: string;
+    tokenType: number;
+  };
+}
+interface Token {
+  createdAt: number;
+  tokenAddress: string;
+  tokenName: string;
+  symbol: string;
+  tokenType: number;
+  parentAddress: string;
+  totalSupply: string;
+  totalTransfers: number;
+  decimals: number;
+  image: null | string;
+  verified: boolean;
+}
+interface KlaytnBalances {
+  success: boolean;
+  code: number;
+  result: {
+    createdAt: number;
+    updatedAt: number;
+    address: string;
+    tokenAddress: string;
+    amount: string;
+  }[];
+  tokens: {
+    [keys: string]: Token;
+  };
 }
 
-export interface Amount {
-  krw: string;
-  usd: string;
-}
-
-export interface Price {
-  krw: string;
-  usd: string;
-}
-
-export interface List {
+interface Coin {
   name: string;
-  assetName: string[];
-  amount: Amount;
-  volume: string[];
-  price: Price[];
+  amount: number;
 }
 
-export interface Pool {
-  message?: string;
-  totalAmount: TotalAmount;
-  list: List[];
+interface Pool {
+  name: string;
+  amountKRW: number;
+  coins: [Coin, Coin];
 }
 
-export interface Staking {
-  message?: string;
-  totalAmount: TotalAmount;
-  list: List[];
-}
-
-export interface Asset {
-  message?: string;
-  totalAmount: TotalAmount;
-  list: List[];
-}
-
-export interface WalletInfo {
-  totalAmount: TotalAmount;
-  pool: Pool;
-  staking: Staking;
-  asset: Asset;
-  reason?: string;
+interface WalletInfo {
+  pools: Pool[];
+  totalAmountKRW: number;
+  totalPoolAmountKRW: number;
 }
 
 class Wallet {
@@ -61,7 +78,7 @@ class Wallet {
 }
 
 @middleware()
-export default class Klaywatch {
+export default class Klaywatch2 {
   private _walletModel: ReturnModelType<typeof Wallet>;
 
   constructor() {
@@ -69,32 +86,111 @@ export default class Klaywatch {
   }
 
   async getWalletInfo(address: string): Promise<WalletInfo> {
-    const res = await axios.get<WalletInfo>(
-      `https://klaywatch.com/api/v1/balance?address=${address}`
+    const tokenCalc = (
+      kslpAmount: number,
+      totalSupply: number,
+      tokenAmount: number
+    ) => {
+      return (kslpAmount / totalSupply) * tokenAmount * 1e-18;
+    };
+    const getMyKSLPAmount = (
+      myBalances: KlaytnBalances,
+      tokenAddress: string
+    ) =>
+      Number.parseInt(
+        myBalances.result.find((el) => el.tokenAddress === tokenAddress).amount
+      );
+    const walletInfo: WalletInfo = {
+      pools: [],
+      totalAmountKRW: 0,
+      totalPoolAmountKRW: 0,
+    };
+    const client = axios.create({
+      baseURL: "https://api-cypress.scope.klaytn.com/v1/",
+    });
+    const { data: myBalances } = await client.get<KlaytnBalances>(
+      `/accounts/${address}/balances`
     );
-    if (res.data.reason !== undefined) throw Error(res.data.reason);
-    return res.data;
+
+    const poolTokens = Object.values(myBalances.tokens).filter(
+      (token) =>
+        token.symbol === "KSLP" &&
+        myBalances.result.find((r) => r.tokenAddress === token.tokenAddress)
+          .amount !== "0"
+    );
+
+    for (const pt of poolTokens) {
+      const { data: poolBalances } = await client.get<KlaytnBalances>(
+        `/accounts/${pt.tokenAddress}/balances`
+      );
+      const m = /KlaySwap LP (\S+)-(\S+)/.exec(pt.tokenName);
+      const coins: [Coin, Coin] = [
+        {
+          name: m[1],
+          amount: 0,
+        },
+        {
+          name: m[2],
+          amount: 0,
+        },
+      ];
+
+      const myKslp = getMyKSLPAmount(myBalances, pt.tokenAddress);
+      if (coins[0].name === "KLAY") {
+        const {
+          data: { result: tokenAddrInfo },
+        } = await client.get<TokenAddressInfo>(`/accounts/${pt.tokenAddress}`);
+        coins[0].amount = tokenCalc(
+          myKslp,
+          Number.parseInt(pt.totalSupply),
+          Number.parseInt(tokenAddrInfo.balance)
+        );
+      } else {
+        coins[0].amount = tokenCalc(
+          myKslp,
+          Number.parseInt(pt.totalSupply),
+          Number.parseInt(
+            poolBalances.result.find(
+              (el) =>
+                poolBalances.tokens[el.tokenAddress].symbol === coins[0].name
+            ).amount
+          )
+        );
+      }
+      coins[1].amount = tokenCalc(
+        myKslp,
+        Number.parseInt(pt.totalSupply),
+        Number.parseInt(
+          poolBalances.result.find(
+            (el) =>
+              poolBalances.tokens[el.tokenAddress].symbol === coins[1].name
+          ).amount
+        )
+      );
+
+      if (coins[0].name === "KUSDT") coins[0].amount *= 1e12;
+      if (coins[1].name === "KUSDT") coins[1].amount *= 1e12;
+
+      walletInfo.pools.push({
+        name: coins[0].name + "-" + coins[1].name,
+        amountKRW: 0,
+        coins,
+      });
+    }
+
+    return walletInfo;
   }
 
   makeText(walletInfo: WalletInfo) {
-    const viewKRW = (krw: string) =>
-      Number.parseInt(krw)
-        .toString()
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "원";
-    const viewNum = (num: string) => Number.parseFloat(num).toFixed(2);
+    const viewNum = (num: number) => num.toFixed(5);
     let poolText = "";
-    for (const pool of walletInfo.pool.list) {
+    for (const pool of walletInfo.pools) {
       poolText += `  ✔️${pool.name}\n`;
       poolText +=
-        `    🪙${pool.assetName[0]}: ${viewNum(pool.volume[0])}\n` +
-        `    🪙${pool.assetName[1]}: ${viewNum(pool.volume[1])}\n` +
-        `    💰예치 자산: ${viewKRW(pool.amount.krw)}\n\n`;
+        `    🥇${pool.coins[0].name}: ${viewNum(pool.coins[0].amount)}\n` +
+        `    🥇${pool.coins[1].name}: ${viewNum(pool.coins[1].amount)}\n\n`;
     }
-    return (
-      `💰총 보유자산\n  ${viewKRW(walletInfo.totalAmount.krw)}\n\n` +
-      `💰Pool 예치 자산\n  ${viewKRW(walletInfo.pool.totalAmount.krw)}\n\n` +
-      `📚Pool 목록\n${poolText}`
-    );
+    return `📚Pool 목록\n${poolText}`;
   }
 
   @command("klaywatch")
